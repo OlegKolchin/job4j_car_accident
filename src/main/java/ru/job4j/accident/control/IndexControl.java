a2b7c856687f9f0dc008175693fff20821264e13
OlegKolchin <commshepard@gmail.com>
2021-12-29 14:29:10 +0500

    2. IndexControl. Таблица и вид. [#2092]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -3,18 +3,28 @@ package ru.job4j.accident.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.repository.AccidentMem;
+import ru.job4j.accident.service.AccidentService;
 
 import java.util.ArrayList;
 
 
 @Controller
 public class IndexControl {
+    private AccidentMem store;
+
+    public IndexControl(AccidentMem store) {
+        Accident accident = new Accident(1, "Accident", "Text", "Address");
+        for (int i = 1; i <= 5; i++) {
+            store.addAccident(i, accident);
+        }
+        this.store = store;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        AccidentMem mem = new AccidentMem();
-        mem.init();
-        model.addAttribute("accidents", new ArrayList<>(mem.getAccidents().values()));
+        model.addAttribute("accidents", new AccidentService().getAccidents(store));
         return "index";
     }
 }
--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -3,24 +3,23 @@ package ru.job4j.accident.repository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public class AccidentMem {
     private HashMap<Integer, Accident> accidents = new HashMap<>();
 
-    public HashMap<Integer, Accident> getAccidents() {
-        return accidents;
+    public Collection<Accident> getAccidents() {
+        return accidents.values();
     }
 
     public void setAccidents(HashMap<Integer, Accident> accidents) {
         this.accidents = accidents;
     }
 
-    public void init() {
-        Accident accident = new Accident(1, "Accident", "Text", "Address");
-        for (int i = 1; i <= 5; i++) {
-            accidents.put(i, accident);
-        }
+    public void addAccident(int i, Accident accident) {
+        accidents.put(i, accident);
     }
 }
