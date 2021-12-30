6eb78a46b710c325e6e2a6437b182ea6015a128e
OlegKolchin <commshepard@gmail.com>
2021-12-30 11:48:06 +0500

    3. @ModelAttribute. Создание инцидента. [#261013 #245205]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -3,22 +3,20 @@ package ru.job4j.accident.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accident.model.Accident;
-import ru.job4j.accident.repository.AccidentMem;
 import ru.job4j.accident.service.AccidentService;
 
 
 @Controller
 public class IndexControl {
-    private AccidentMem store;
+    private AccidentService service;
 
-    public IndexControl(AccidentMem store) {
-        this.store = store;
+    public IndexControl(AccidentService service) {
+        this.service = service;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("accidents", new AccidentService(store).getAccidents());
+        model.addAttribute("accidents", service.getAccidents());
         return "index";
     }
 }
