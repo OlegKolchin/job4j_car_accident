b2e88894a8e50e76d6bbab15ba18298e7c461a56
OlegKolchin <commshepard@gmail.com>
2022-01-12 12:09:01 +0500

    2. Spring Data [#296073 #249412]

--- src/main/java/ru/job4j/accident/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accident/repository/AccidentRepository.java
@@ -1,7 +1,11 @@
 package ru.job4j.accident.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accident.model.Accident;
 
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
+
+    @Query("from Accident a join fetch a.rules")
+    Iterable<Accident> findAll();
 }
