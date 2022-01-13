d692e523c06c1f772960a8211256090588b6dcf9
OlegKolchin <commshepard@gmail.com>
2022-01-13 12:25:16 +0500

    2. Регистрация пользователя [#296069 #249992]

--- src/main/java/ru/job4j/accident/control/RegControl.java
+++ src/main/java/ru/job4j/accident/control/RegControl.java
@@ -1,11 +1,12 @@
 package ru.job4j.accident.control;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.model.User;
 import ru.job4j.accident.repository.AuthorityRepository;
 import ru.job4j.accident.repository.UserRepository;
@@ -24,11 +25,18 @@ public class RegControl {
     }
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
-        user.setEnabled(true);
-        user.setPassword(encoder.encode(user.getPassword()));
-        user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
+    public String regSave(@ModelAttribute User user, Model model) {
+        try {
+            user.setEnabled(true);
+            user.setPassword(encoder.encode(user.getPassword()));
+            user.setAuthority(authorities.findByAuthority("ROLE_USER"));
+            users.save(user);
+        } catch (Exception e) {
+            if (e.getClass().equals(DataIntegrityViolationException.class)) {
+                model.addAttribute("errorMessage", "Username already exists!");
+                return "reg";
+            }
+        }
         return "redirect:/login";
     }
 
